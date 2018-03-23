b7b006a5a781a32f0af77410fcf57ced644a3eda
Alex <AlexGold1994@gmail.com>
2018-03-24 00:48:27 +0200

    Header ended

--- app/js/common.js
+++ app/js/common.js
@@ -1,5 +1,38 @@
-$(function() {
+$(function () {
 
-	// Custom JS
+	function heightDetect() {
+		$(".main__head").css("height", $(window).height());
+	};
+	heightDetect();
+	$(window).resize(function () {
+		heightDetect();
+	});
 
-});
+	$(".toggle_mnu").click(function () {
+		$(".sandwich").toggleClass("active");
+	});
+
+	$(".top_mnu ul a").click(function () {
+		$(".top_mnu").fadeOut(600);
+		$(".sandwich").toggleClass("active");
+		$(".top_text").css("opacity", "1");
+	}).append("<span>");
+
+	$(".toggle_mnu").click(function () {
+		if ($(".top_mnu").is(":visible")) {
+			$(".top_text").css("opacity", "1");
+			$(".top_mnu").fadeOut(600);
+			$(".top_mnu li a").removeClass("fadeInUp animated");
+		} else {
+			$(".top_text").css("opacity", ".1");
+			$(".top_mnu").fadeIn(600);
+			$(".top_mnu li a").addClass("fadeInUp animated");
+		};
+	});
+
+$(window).on("load", function () {
+
+	$(".loader_inner").fadeOut();
+	$(".loader").delay(400).fadeOut("slow");
+}); 
+}); 
--- gulpfile.js
+++ gulpfile.js
@@ -35,7 +35,20 @@ gulp.task('sass', function() {
 gulp.task('js', function() {
 	return gulp.src([
 		'app/libs/jquery/dist/jquery.min.js',
-		'app/js/common.js', // Always at the end
+		'app/libs/parallax/parallax.min.js',
+		'app/libs/waypoints/waypoints.min.js',
+		'app/libs/animate/animate-css.js',
+		/*'app/libs/html5shiv/es5-shim.min.js',
+		'app/libs/html5shiv/html5shiv-printshiv.min.js',
+		'app/libs/html5shiv/html5shiv.min.js',
+		'app/libs/html5shiv/respond.min.js',*/
+		'app/libs/jqBootstrapValidation/jqBootstrapValidation.js',
+		'app/libs/magnific-popup/dist/jquery.magnific-popup.min.js',
+		'app/libs/mixitup/mixitup.min.js',
+		'app/libs/respond/respond.min.js',
+		'app/libs/scroll2id/PageScroll2id.min.js',
+		'app/js/common.js', 
+		// Always at the end
 		])
 	.pipe(concat('scripts.min.js'))
 	// .pipe(uglify()) // Mifify js (opt.)
